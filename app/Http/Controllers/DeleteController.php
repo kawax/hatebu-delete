4748f647b20565803343d8d7b7814d4439858d70
kawax <kawaxbiz@gmail.com>
2018-06-16 18:52:25 +0900

    del

--- app/Http/Controllers/DeleteController.php
+++ app/Http/Controllers/DeleteController.php
@@ -8,6 +8,11 @@ use App\Jobs\DeleteJob;
 
 class DeleteController extends Controller
 {
+    /**
+     * @param Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
     public function __invoke(Request $request)
     {
         DeleteJob::dispatchNow($request->user());
