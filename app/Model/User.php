07b9ab306cf722862236723f86ce7420c109d1dd
kawax <kawaxbiz@gmail.com>
2018-09-05 11:15:52 +0900

    L5.7

--- app/Model/User.php
+++ app/Model/User.php
@@ -2,8 +2,10 @@
 
 namespace App\Model;
 
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 
 class User extends Authenticatable
 {
