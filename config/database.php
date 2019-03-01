a801f7dc223a2eafa047e609add47b7aaa3b15e5
kawax <kawaxbiz@gmail.com>
2019-03-01 21:13:10 +0900

    db

--- config/database.php
+++ config/database.php
@@ -52,9 +52,9 @@ return [
             'prefix'      => '',
             'strict'      => true,
             'engine'      => null,
-            //            'options' => array_filter([
-            //                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            //            ]),
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
         ],
 
         'pgsql' => [
