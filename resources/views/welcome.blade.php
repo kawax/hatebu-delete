1bf766127b3330d52704212fe49ac3e4bfa91ecb
kawax <kawaxbiz@gmail.com>
2020-01-22 21:05:06 +0900

    view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,12 @@
                         <p>意外とユーザーがいるのでお知らせ欄。</p>
                         <hr>
                         <p>
+                            2020/01/22 やっと全削除機能が再開された。
+                            <a href="https://bookmark.hatenastaff.com/entry/2020/01/22/165413" target="_blank">ブックマークの全削除機能の提供を再開しました
+                                - はてなブックマーク開発ブログ</a>
+                        </p>
+                        <hr>
+                        <p>
                             2019/08/29 <a href="https://bookmark.hatenastaff.com/entry/2019/08/26/111011"
                                           target="_blank">HTTPS切り替え対応済</a>。
                         </p>
