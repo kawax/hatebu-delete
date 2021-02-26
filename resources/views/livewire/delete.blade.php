8af1e29016645da299a5891af8facc7c5bb6a4c7
kawax <kawaxbiz@gmail.com>
2021-02-26 20:45:25 +0900

    loading

--- resources/views/livewire/delete.blade.php
+++ resources/views/livewire/delete.blade.php
@@ -1,6 +1,10 @@
 <div class="card border-primary">
     <div class="card-body">
-        <button wire:click="delete()" class="btn btn-primary">最近の20件を削除</button>
+        <button wire:click="delete()"
+                class="btn btn-primary"
+                wire:loading.attr="disabled">
+            最近の20件を削除
+        </button>
 
         <small class="text-muted">（{{ config('hatena.delete_days') }}日以内のブックマークは除く）</small>
     </div>
