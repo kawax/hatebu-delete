8ecf90516e5adeda11a7e463aafd8ab6983b71ad
kawax <kawaxbiz@gmail.com>
2021-03-29 17:44:33 +0900

    wip

--- config/queue.php
+++ config/queue.php
@@ -67,6 +67,7 @@ return [
             'queue' => env('REDIS_QUEUE', 'default'),
             'retry_after' => 90,
             'block_for' => null,
+            'after_commit' => true,
         ],
 
     ],
