d0ef669cd1536efb890962012dc47ff5741bbd62
kawax <kawaxbiz@gmail.com>
2021-04-14 19:34:41 +0900

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -33,7 +33,7 @@
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+                <ul class="navbar-nav me-auto">
                     @auth
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('home') }}">{{ __('ホーム') }}</a>
