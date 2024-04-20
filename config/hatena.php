0a92f60788a922c5340489a2353cb3f86a806638
kawax <kawaxbiz@gmail.com>
2024-04-20 16:56:44 +0900

    Update hatena.php

--- config/hatena.php
+++ config/hatena.php
@@ -3,8 +3,7 @@
 use function Revolution\Illuminate\Support\env;
 
 return [
-    'key' => env('SPECIAL_KEY'),
-    'delete_days' => env('DELETE_DAYS', 3),
+    'delete_days' => (int) env('DELETE_DAYS', 3),
     'analytics' => env('GOOGLE_ANALYTICS'),
 
     'users' => explode(',', env('USERS')),
