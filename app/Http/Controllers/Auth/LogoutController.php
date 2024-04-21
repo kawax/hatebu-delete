24e2d571ce646e57c07e2d112e6570850a35d965
kawax <kawaxbiz@gmail.com>
2024-04-21 13:19:30 +0900

    Create LogoutController.php

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function __invoke(Request $request): RedirectResponse
+    {
+        auth()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return to_route('welcome');
+    }
+}
