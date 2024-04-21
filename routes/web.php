77bb6179373de0023159ac4d4ff2c9eaf52c850e
kawax <kawaxbiz@gmail.com>
2024-04-21 13:16:28 +0900

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('welcome');
 
-Route::middleware('guest')->group(function () {
-    Route::get('login', [LoginController::class, 'login'])->name('login');
-    Route::get('callback', [LoginController::class, 'callback'])->name('callback');
-});
+Route::middleware('guest')
+    ->controller(LoginController::class)
+    ->group(function () {
+        Route::get('login', 'login')->name('login');
+        Route::get('callback', 'callback')->name('callback');
+    });
 
 Route::middleware('auth')->group(function () {
     Route::view('home', 'home')->name('home');
