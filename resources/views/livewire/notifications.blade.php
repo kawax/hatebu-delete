d7c474cc0c7b4b0715612e820e632ad0f661cee7
kawax <kawaxbiz@gmail.com>
2024-05-08 12:19:29 +0900

    wip

--- resources/views/livewire/notifications.blade.php
+++ resources/views/livewire/notifications.blade.php
@@ -11,7 +11,7 @@
                 @switch(class_basename($notification->type))
                     @case('DeleteNotification')
                         <a href="{{ data_get($notification->data, 'url') }}" target="_blank"
-                           class="font-bold text-indigo-500 hover:text-indigo-800 underline">
+                           class="font-bold text-indigo-500 hover:text-indigo-800 underline break-all">
                             {{ data_get($notification->data, 'title') }}
                         </a>
                         を削除。
