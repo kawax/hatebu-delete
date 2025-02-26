fb89a1b172e77e5ae287e74c0c25f654ed44d07d
kawax <kawaxbiz@gmail.com>
2025-02-26 14:35:17 +0900

    Laravel 12

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,9 @@ class AppServiceProvider extends ServiceProvider
         TallStackUi::personalize()
             ->card()
             ->block('wrapper.second', 'dark:bg-dark-700 flex w-full flex-col bg-white shadow-md')
-            ->block('header.wrapper', 'dark:border-b-dark-600 flex items-center justify-between border-b border-b-gray-100 p-4
+            ->block('header.wrapper.base', 'dark:border-b-dark-600 flex items-center justify-between border-b border-b-gray-100 p-4
  bg-indigo-500')
-            ->block('header.text', 'text-md text-white dark:text-dark-300');
+            ->block('header.text.color', 'text-md text-white dark:text-dark-300');
     }
 
     /**
