dae5d37b243d3729181dc159e934b60fd293bef7
kawax <kawaxbiz@gmail.com>
2025-05-09 11:50:37 +0900

    Update card.blade.php

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,9 +1,9 @@
-<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-800 shadow sm:rounded-lg p-6']) }}>
+<div {{ $attributes->merge(['class' => 'bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg p-6']) }}>
     @if (isset($header))
         <div class="mb-4">
             {{ $header }}
         </div>
     @endif
-    
+
     {{ $slot }}
 </div>
