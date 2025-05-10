36655795484ea3b5933f13b6202c278795a15fdb
kawax <kawaxbiz@gmail.com>
2025-05-10 14:57:15 +0900

    wip

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,8 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @fluxAppearance
+
 </head>
 <body>
 <div>
@@ -31,6 +33,6 @@
         </div>
     </main>
 </div>
-
+@fluxScripts
 </body>
 </html>
