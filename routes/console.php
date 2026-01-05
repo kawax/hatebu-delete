2a2dd68d98986161eb397412beea4f6c4cddfa3e
kawax <kawaxbiz@gmail.com>
2026-01-05 10:28:37 +0900

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -9,4 +9,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command(DeleteCommand::class)->hourly();
+// Schedule::command(DeleteCommand::class)->hourly();
