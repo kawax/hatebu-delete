b7f35f77ed53bbdb1f1e014e4b5459b31a645c5c
kawax <kawaxbiz@gmail.com>
2026-01-14 15:51:59 +0900

    Create 2026_01_14_151513_add_index_to_cache.php

---
+++ database/migrations/2026_01_14_151513_add_index_to_cache.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cache', function (Blueprint $table) {
+            $table->index(['expiration']);
+        });
+
+        Schema::table('cache_locks', function (Blueprint $table) {
+            $table->index(['expiration']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cache', function (Blueprint $table) {
+            $table->dropIndex(['expiration']);
+        });
+
+        Schema::table('cache_locks', function (Blueprint $table) {
+            $table->dropIndex(['expiration']);
+        });
+    }
+};
